51fa86ca3975ac45ed74cd681f6a2c02558a94a5
David Palacios <davidpal3c@gmail.com>
2024-05-14 18:42:35 -0600

    changed user registration from class-based to function-based to accomodate AJAX

--- backend/config/user_auth/views.py
+++ backend/config/user_auth/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.urls import reverse_lazy
-from django.views.decorators.csrf import csrf_protect
 from .forms import RegisterForm, LoginForm
 from django.views.generic import CreateView, FormView
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+import json
+
+
 
 
 from .models import User
@@ -37,33 +40,48 @@ def logout_user(request):
     return redirect('home')
 
 
- 
-class RegisterView(CreateView):
-    form_class = RegisterForm
-    template_name = 'userauths/register.html'
-    success_url = '/'
-    success_url = reverse_lazy('home')
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f"Welcome {user.email}!")
+            return redirect(reverse_lazy('home'))
+        else:
+            messages.error(request, "Registration failed. Please try again.")
+    else:
+        form = RegisterForm()
+    return render(request, 'userauths/register.html', {'form': form})
 
 
-    def form_valid(self, form):
-        password = form.cleaned_data['password']
-        password_2 = form.cleaned_data['password_2']
+
+# class RegisterView(CreateView):
+#     form_class = RegisterForm
+#     template_name = 'userauths/register.html'
+#     success_url = '/'
+#     success_url = reverse_lazy('home')
+
+
+#     def form_valid(self, form):
+#         password = form.cleaned_data['password']
+#         password_2 = form.cleaned_data['password_2']
         
-        if password != password_2:
-            messages.success(self.request, "The passwords do not match. Please try again.")
-            # form.add_error(None, "The passwords do not match. Please try again.")
-            return self.form_invalid(form)
+#         if password != password_2:
+#             messages.success(self.request, "The passwords do not match. Please try again.")
+#             # form.add_error(None, "The passwords do not match. Please try again.")
+#             return self.form_invalid(form)
 
-        response = super().form_valid(form)
-        user = form.save()
-        login(self.request, user)
-        messages.success(self.request, f"Welcome {user.email}!")
-        return response
+#         response = super().form_valid(form)
+#         user = form.save()
+#         login(self.request, user)
+#         messages.success(self.request, f"Welcome {user.email}!")
+#         return response
 
 
-    def form_invalid(self, form):
-        # messages.success(self.request, "The password do not match. Please try again.")
-        return super().form_invalid(form)
+#     def form_invalid(self, form):
+#         # messages.success(self.request, "The password do not match. Please try again.")
+#         return super().form_invalid(form)
 
 
 
